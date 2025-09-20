65fcc8fb7586c66284b995684fc8fb2fa4cff6ff
ineskhou <ineskhouider03@gmail.com>
2025-09-20 13:21:27 -0700

    put m0p back

--- boards/bronco_space/proves_flight_control_board_v4b/board.cmake
+++ boards/bronco_space/proves_flight_control_board_v4b/board.cmake
@@ -13,18 +13,10 @@ board_runner_args(openocd --cmd-pre-init "source [find interface/${RPI_PICO_DEBU
 # https://www.raspberrypi.com/documentation/microcontrollers/debug-probe.html#debugging-with-swd
 board_runner_args(openocd --cmd-pre-init "set_adapter_speed_if_not_set 5000")
 
-if(BOARD MATCHES "^proves_flight_control_board_v4")
-    board_runner_args(openocd --cmd-pre-init "source [find target/rp2040.cfg]")
-    board_runner_args(jlink "--device=RP2040_M0_0")
-    board_runner_args(uf2 "--board-id=RP2040")
-elseif(BOARD MATCHES "^proves_flight_control_board_v5")
-    board_runner_args(openocd --cmd-pre-init "source [find target/rp2340.cfg]")
-    board_runner_args(jlink "--device=RP2340_M0_0")
-    board_runner_args(uf2 "--board-id=RP2340")
-endif()
 
-
-
+board_runner_args(openocd --cmd-pre-init "source [find target/rp2040.cfg]")
+board_runner_args(jlink "--device=RP2040_M0_0")
+board_runner_args(uf2 "--board-id=RP2040")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
