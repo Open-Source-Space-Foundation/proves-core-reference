a7171170a8a28b2ad552a71b6959023e0252597c
ineskhou <ineskhouider03@gmail.com>
2025-10-08 15:41:29 -0700

    remover reset

--- FprimeZephyrReference/test/int/burnwire_test.py
+++ FprimeZephyrReference/test/int/burnwire_test.py
@@ -11,7 +11,7 @@ from fprime_gds.common.testing_fw.api import IntegrationTestAPI
 
 
 @pytest.fixture(autouse=True)
-def reset_burnwire(fprime_test_api: IntegrationTestAPI, start_gds):
+def reset_burnwire(fprime_test_api: IntegrationTestAPI):
     """Fixture to stop burnwire and clear histories before/after each test"""
     # Stop burnwire and clear before test
     fprime_test_api.clear_histories()
