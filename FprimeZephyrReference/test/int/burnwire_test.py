e3e7257f0a5e66bcd760dfd3161c8aa9932191bb
ineskhou <ineskhouider03@gmail.com>
2025-10-08 02:00:28 -0700

    remove command box

--- FprimeZephyrReference/test/int/burnwire_test.py
+++ FprimeZephyrReference/test/int/burnwire_test.py
@@ -24,18 +24,14 @@ def reset_burnwire(fprime_test_api: IntegrationTestAPI):
 
 
 def stop_burnwire(fprime_test_api: IntegrationTestAPI):
-    fprime_test_api.send_and_assert_command(
-        f"{COMPONENT}.STOP_BURNWIRE", [], max_delay=2
-    )
+    fprime_test_api.send_and_assert_command(f"{COMPONENT}.STOP_BURNWIRE", max_delay=2)
 
 
 def test_01_start_and_stop_burnwire(fprime_test_api: IntegrationTestAPI):
     """Test that burnwire starts and stops as expected"""
 
     # Start burnwire
-    fprime_test_api.send_and_assert_command(
-        f"{COMPONENT}.START_BURNWIRE", [], max_delay=2
-    )
+    fprime_test_api.send_and_assert_command(f"{COMPONENT}.START_BURNWIRE", max_delay=2)
 
     # Wait for SetBurnwireState = ON
     fprime_test_api.assert_event(f"{COMPONENT}.SetBurnwireState", ["ON"], timeout=2)
@@ -48,17 +44,13 @@ def test_02_manual_stop_before_timeout(fprime_test_api: IntegrationTestAPI):
     """Test that burnwire stops manually before the safety timer expires"""
 
     # Start burnwire
-    fprime_test_api.send_and_assert_command(
-        f"{COMPONENT}.START_BURNWIRE", [], max_delay=2
-    )
+    fprime_test_api.send_and_assert_command(f"{COMPONENT}.START_BURNWIRE", max_delay=2)
 
     # Confirm Burnwire turned ON
     fprime_test_api.assert_event(f"{COMPONENT}.SetBurnwireState", ["ON"], timeout=2)
 
     # # Stop burnwire before safety timer triggers
-    fprime_test_api.send_and_assert_command(
-        f"{COMPONENT}.STOP_BURNWIRE", [], max_delay=2
-    )
+    fprime_test_api.send_and_assert_command(f"{COMPONENT}.STOP_BURNWIRE", max_delay=2)
 
     # Confirm Burnwire turned OFF
     fprime_test_api.assert_event(f"{COMPONENT}.SetBurnwireState", ["OFF"], timeout=2)
