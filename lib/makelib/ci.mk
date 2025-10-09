9ec2d0cbf611523b6c6c2dd918d023652e15d13b
Nate Gay <email@nategay.me>
2025-10-09 01:07:32 -0500

    Simplifying

--- lib/makelib/ci.mk
+++ lib/makelib/ci.mk
@@ -1,11 +1,3 @@
 .PHONY: minimize-uv-cache
 minimize-uv-cache:
 	@$(UV) cache prune --ci
-
-.PHONY: generate-ci
-generate-ci:
-	@$(UV_RUN) fprime-util generate -f
-
-.PHONY: build-ci
-build-ci:
-	@$(UV_RUN) fprime-util build
