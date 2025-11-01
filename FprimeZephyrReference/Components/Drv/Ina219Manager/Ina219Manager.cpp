0c5314b5f9d496b444c633f059f920f9a2a58264
asiemsen <siemsenaaron@gmail.com>
2025-11-01 17:03:47 -0500

    implemented ina219 manager cpp

--- FprimeZephyrReference/Components/Drv/Ina219Manager/Ina219Manager.cpp
+++ FprimeZephyrReference/Components/Drv/Ina219Manager/Ina219Manager.cpp
@@ -1,4 +1,79 @@
 // ======================================================================
 // \title  Ina219Manager.cpp
-// \brief  Manager for Ina219 device placeholder. Use fprime-util impl
+// \author nate
+// \brief  cpp file for Ina219Manager component implementation class
 // ======================================================================
+
+#include "FprimeZephyrReference/Components/Drv/Ina219Manager/Ina219Manager.hpp"
+
+namespace Drv {
+
+// ----------------------------------------------------------------------
+// Component construction and destruction
+// ----------------------------------------------------------------------
+
+Ina219Manager ::Ina219Manager(const char* const compName) : Ina219ManagerComponentBase(compName) {
+    dev = device_get_binding("INA219");
+}
+
+Ina219Manager ::~Ina219Manager() {}
+
+// ----------------------------------------------------------------------
+// Handler implementations for typed input ports
+// ----------------------------------------------------------------------
+
+F64 Ina219Manager ::currentGet_handler(FwIndexType portNum) {
+    if(!device_is_ready(dev)) {
+        this->log_WARNING_HI_DeviceNotReady();
+        return 0;
+    }
+    this->log_WARNING_HI_DeviceNotReady_ThrottleClear();
+
+    struct sensor_value current; 
+
+    sensor_sample_fetch_chan(dev, SENSOR_CHAN_CURRENT);
+
+    sensor_channel_get(dev, SENSOR_CHAN_CURRENT, &current);
+
+    this->tlmWrite_Current(sensor_value_to_double(&current));
+
+    return current;
+}
+
+F64 Ina219Manager ::powerGet_handler(FwIndexType portNum) {
+    if(!device_is_ready(dev)) {
+        this->log_WARNING_HI_DeviceNotReady();
+        return 0;
+    }
+    this->log_WARNING_HI_DeviceNotReady_ThrottleClear();
+
+    struct sensor_value power; 
+
+    sensor_sample_fetch_chan(dev, SENSOR_CHAN_POWER);
+
+    sensor_channel_get(dev, SENSOR_CHAN_POWER, &Power);
+
+    this->tlmWrite_Current(sensor_value_to_double(&power));
+    
+    return power;
+}
+
+F64 Ina219Manager ::voltageGet_handler(FwIndexType portNum) {
+    if(!device_is_ready(dev)) {
+        this->log_WARNING_HI_DeviceNotReady();
+        return 0;
+    }
+    this->log_WARNING_HI_DeviceNotReady_ThrottleClear();
+
+    struct sensor_value voltage; 
+
+    sensor_sample_fetch_chan(dev, SENSOR_CHAN_VOLTAGE);
+
+    sensor_channel_get(dev, SENSOR_CHAN_VOLTAGE, &voltage);
+
+    this->tlmWrite_Current(sensor_value_to_double(&voltage));
+    
+    return voltage;
+}
+
+}  // namespace Drv
--- FprimeZephyrReference/Components/Drv/Ina219Manager/Ina219Manager.hpp
+++ FprimeZephyrReference/Components/Drv/Ina219Manager/Ina219Manager.hpp
@@ -1,4 +1,59 @@
 // ======================================================================
 // \title  Ina219Manager.hpp
-// \brief  Manager for Ina219 device placeholder. Use fprime-util impl
+// \author nate
+// \brief  hpp file for Ina219Manager component implementation class
 // ======================================================================
+
+#ifndef Drv_Ina219Manager_HPP
+#define Drv_Ina219Manager_HPP
+
+#include "FprimeZephyrReference/Components/Drv/Ina219Manager/Ina219ManagerComponentAc.hpp"
+
+namespace Drv {
+
+class Ina219Manager final : public Ina219ManagerComponentBase {
+  public:
+    // ----------------------------------------------------------------------
+    // Component construction and destruction
+    // ----------------------------------------------------------------------
+
+    //! Construct Ina219Manager object
+    Ina219Manager(const char* const compName  //!< The component name
+    );
+
+    //! Destroy Ina219Manager object
+    ~Ina219Manager();
+
+  private:
+    // ----------------------------------------------------------------------
+    // Handler implementations for typed input ports
+    // ----------------------------------------------------------------------
+
+    //! Handler implementation for currentGet
+    //!
+    //! Port to read the current in amps
+    F64 currentGet_handler(FwIndexType portNum  //!< The port number
+                           ) override;
+
+    //! Handler implementation for powerGet
+    //!
+    //! Port to read the power in watts
+    F64 powerGet_handler(FwIndexType portNum  //!< The port number
+                         ) override;
+
+    //! Handler implementation for voltageGet
+    //!
+    //! Port to read the voltage in volts
+    F64 voltageGet_handler(FwIndexType portNum  //!< The port number
+                           ) override;
+    // ----------------------------------------------------------------------
+    // Member variables
+    // ----------------------------------------------------------------------
+
+    //! Zephyr device stores the initialized LIS2MDL sensor
+    const struct device* dev;                    
+};
+
+}  // namespace Drv
+
+#endif
