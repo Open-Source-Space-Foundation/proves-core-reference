08290365210930def241d6f05eb3464341e1752f
Nate Gay <email@nategay.me>
2025-11-02 17:41:07 -0600

    Refactor IMU Manager test for speed

--- FprimeZephyrReference/test/int/imu_manager_test.py
+++ FprimeZephyrReference/test/int/imu_manager_test.py
@@ -4,8 +4,11 @@ imu_manager_test.py:
 Integration tests for the IMU Manager component.
 """
 
+from datetime import datetime
+
 import pytest
 from common import proves_send_and_assert_command
+from fprime.common.models.serialize.time_type import TimeType
 from fprime_gds.common.data_types.ch_data import ChData
 from fprime_gds.common.testing_fw.api import IntegrationTestAPI
 
@@ -25,17 +28,18 @@ def send_packet(fprime_test_api: IntegrationTestAPI, start_gds):
 
 def test_01_imu_telemetry(fprime_test_api: IntegrationTestAPI, start_gds):
     """Test that we can get IMU telemetry"""
+    start: TimeType = TimeType().set_datetime(datetime.now())
     result_acceleration: ChData = fprime_test_api.assert_telemetry(
-        f"{lsm6dsoManager}.Acceleration", start="NOW", timeout=65
+        f"{lsm6dsoManager}.Acceleration", start=start, timeout=65
     )
     result_angular_velocity: ChData = fprime_test_api.assert_telemetry(
-        f"{lsm6dsoManager}.AngularVelocity", start="NOW", timeout=65
+        f"{lsm6dsoManager}.AngularVelocity", start=start, timeout=65
     )
     result_temperature: ChData = fprime_test_api.assert_telemetry(
-        f"{lsm6dsoManager}.Temperature", start="NOW", timeout=65
+        f"{lsm6dsoManager}.Temperature", start=start, timeout=65
     )
     result_magnetic_field: ChData = fprime_test_api.assert_telemetry(
-        f"{lis2mdlManager}.MagneticField", start="NOW", timeout=65
+        f"{lis2mdlManager}.MagneticField", start=start, timeout=65
     )
 
     reading_acceleration: dict[float] = result_acceleration.get_val()
