24e3968c742d0dc5b33acda64ff90171314c40d3
ineskhou <ik2512@columbia.edu>
2025-11-04 00:35:21 -0800

    started backwards componetn thing

---
+++ FprimeZephyrReference/Components/Authenticator/Authenticator.cpp
@@ -0,0 +1,4 @@
+// ======================================================================
+// \title  Authenticator.cpp
+// \brief  This adds Authentication to components for downlink placeholder. Use fprime-util impl
+// ======================================================================
---
+++ FprimeZephyrReference/Components/Authenticator/Authenticator.hpp
@@ -0,0 +1,4 @@
+// ======================================================================
+// \title  Authenticator.hpp
+// \brief  This adds Authentication to components for downlink placeholder. Use fprime-util impl
+// ======================================================================
