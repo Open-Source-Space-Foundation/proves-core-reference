33453836244f101dcd64d4206627b0f849b818b7
ineskhou <ineskhouider03@gmail.com>
2025-11-11 16:51:53 -0800

    switched the telem packets

--- FprimeZephyrReference/test/int/load_switch_test.py
+++ FprimeZephyrReference/test/int/load_switch_test.py
@@ -41,7 +41,7 @@ def get_is_on(fprime_test_api: IntegrationTestAPI) -> str:
     proves_send_and_assert_command(
         fprime_test_api,
         "CdhCore.tlmSend.SEND_PKT",
-        ["9"],
+        ["10"],
     )
     result: ChData = fprime_test_api.assert_telemetry(
         f"{loadswitch}.IsOn", start="NOW", timeout=3
