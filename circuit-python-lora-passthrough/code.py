61d992b14b396ddede6880523debf091a767bf1f
M Starch <LeStarch@googlemail.com>
2025-12-12 15:43:15 -0800

    Update code

--- circuit-python-lora-passthrough/code.py
+++ circuit-python-lora-passthrough/code.py
@@ -5,8 +5,6 @@ This code will forward any received LoRa packets to the serial console (sys.stdo
 to indicate packet reception.
 """
 
-import time
-
 import adafruit_rfm9x
 import board
 import digitalio
@@ -14,15 +12,14 @@ import usb_cdc
 
 # Radio constants
 RADIO_FREQ_MHZ = 437.4
-CS = digitalio.DigitalInOut(board.SPI0_CS0)
-RESET = digitalio.DigitalInOut(board.RF1_RST)
+CS = digitalio.DigitalInOut(board.RFM9X_CS)
+RESET = digitalio.DigitalInOut(board.RFM9X_RST)
 
 rfm95 = adafruit_rfm9x.RFM9x(board.SPI(), CS, RESET, RADIO_FREQ_MHZ)
 rfm95.spreading_factor = 8
 rfm95.signal_bandwidth = 125000
 rfm95.coding_rate = 5
 rfm95.preamble_length = 8
-time_start = time.time()
 packet_count = 0
 print("[INFO] LoRa Receiver receiving packets")
 while True:
@@ -32,10 +29,15 @@ while True:
     if packet is not None:
         usb_cdc.data.write(packet)
         packet_count += 1
-    time_delta = time.time() - time_start
-    if time_delta > 10:
-        print(f"[INFO] Packets received: {packet_count}")
-        time_start = time.time()
+    data = usb_cdc.console.read(usb_cdc.data.in_waiting)
+    try:
+        rate = int(data)
+        if rate < 7 or rate > 9:
+            raise ValueError("Spreading factor must be between 7 and 9")
+        print(f"[INFO] Setting spreading factor to {rate}")
+        rfm95.spreading_factor = rate
+    except (ValueError, TypeError):
+        pass
     data = usb_cdc.data.read(usb_cdc.data.in_waiting)
     if len(data) > 0:
         rfm95.send(data)
