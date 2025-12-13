43905176f6df4dfa2180ed59c60203d8a9aa5cf2
Michael Pham <61564344+Mikefly123@users.noreply.github.com>
2025-12-13 08:07:26 -0600

    Merged in Main Stragglers

---
+++ FprimeZephyrReference/Components/Drv/Tmp112Manager/Tmp112Manager.cpp
@@ -0,0 +1,179 @@
+// ======================================================================
+// \title  Tmp112Manager.cpp
+// \brief  cpp file for Tmp112Manager component implementation class
+// ======================================================================
+
+#include "FprimeZephyrReference/Components/Drv/Tmp112Manager/Tmp112Manager.hpp"
+
+namespace Drv {
+
+// ----------------------------------------------------------------------
+// Component construction and destruction
+// ----------------------------------------------------------------------
+
+Tmp112Manager ::Tmp112Manager(const char* const compName) : Tmp112ManagerComponentBase(compName) {}
+
+Tmp112Manager ::~Tmp112Manager() {}
+
+// ----------------------------------------------------------------------
+// Public helper methods
+// ----------------------------------------------------------------------
+
+void Tmp112Manager::configure(const struct device* tca,
+                              const struct device* mux,
+                              const struct device* dev,
+                              bool loadSwitchCheck) {
+    this->m_tca = tca;
+    this->m_mux = mux;
+    this->m_dev = dev;
+    // TODO(nateinaction): Abstract load switch check, perhaps wrap this component and only use when needed?
+    this->m_load_switch_check = loadSwitchCheck;
+}
+
+// ----------------------------------------------------------------------
+// Handler implementations for typed input ports
+// ----------------------------------------------------------------------
+
+Fw::Success Tmp112Manager ::loadSwitchStateChanged_handler(FwIndexType portNum, const Fw::On& state) {
+    // Store the load switch state
+    this->m_load_switch_state = state;
+
+    // If the load switch is off, deinitialize the device
+    if (this->m_load_switch_state == Fw::On::OFF) {
+        return this->deinitializeDevice();
+    }
+
+    // If the load switch is on, set the timeout
+    // We only consider the load switch to be fully on after a timeout period
+    this->m_load_switch_on_timeout = this->getTime();
+    this->m_load_switch_on_timeout.add(1, 0);
+
+    return Fw::Success::SUCCESS;
+}
+
+F64 Tmp112Manager ::temperatureGet_handler(FwIndexType portNum, Fw::Success& condition) {
+    condition = Fw::Success::FAILURE;
+
+    if (!this->initializeDevice()) {
+        return 0;
+    }
+
+    int rc = sensor_sample_fetch_chan(this->m_dev, SENSOR_CHAN_AMBIENT_TEMP);
+    if (rc != 0) {
+        this->log_WARNING_LO_SensorSampleFetchFailed(rc);
+        return 0;
+    }
+    this->log_WARNING_LO_SensorSampleFetchFailed_ThrottleClear();
+
+    struct sensor_value val;
+    rc = sensor_channel_get(this->m_dev, SENSOR_CHAN_AMBIENT_TEMP, &val);
+    if (rc != 0) {
+        this->log_WARNING_LO_SensorChannelGetFailed(rc);
+        return 0;
+    }
+    this->log_WARNING_LO_SensorChannelGetFailed_ThrottleClear();
+
+    F64 temp = sensor_value_to_double(&val);
+
+    this->tlmWrite_Temperature(temp);
+
+    condition = Fw::Success::SUCCESS;
+    return temp;
+}
+
+// ----------------------------------------------------------------------
+// Handler implementations for commands
+// ----------------------------------------------------------------------
+
+void Tmp112Manager ::GetTemperature_cmdHandler(FwOpcodeType opCode, U32 cmdSeq) {
+    Fw::Success condition;
+    F64 temperature = this->temperatureGet_handler(0, condition);
+    if (condition != Fw::Success::SUCCESS) {
+        this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::EXECUTION_ERROR);
+        return;
+    }
+    this->log_ACTIVITY_HI_Temperature(temperature);
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+}
+
+// ----------------------------------------------------------------------
+// Private helper methods
+// ----------------------------------------------------------------------
+
+bool Tmp112Manager ::isDeviceInitialized() {
+    if (!this->m_dev) {
+        this->log_WARNING_LO_DeviceNil();
+        return false;
+    }
+    this->log_WARNING_LO_DeviceNil_ThrottleClear();
+
+    if (!this->m_dev->state) {
+        this->log_WARNING_LO_DeviceStateNil();
+        return false;
+    }
+    this->log_WARNING_LO_DeviceStateNil_ThrottleClear();
+
+    return this->m_dev->state->initialized;
+}
+
+Fw::Success Tmp112Manager ::initializeDevice() {
+    if (this->isDeviceInitialized()) {
+        if (!device_is_ready(this->m_dev)) {
+            this->log_WARNING_LO_DeviceNotReady();
+            return Fw::Success::FAILURE;
+        }
+        this->log_WARNING_LO_DeviceNotReady_ThrottleClear();
+        return Fw::Success::SUCCESS;
+    }
+
+    if (!device_is_ready(this->m_tca)) {
+        this->log_WARNING_LO_TcaUnhealthy();
+        return Fw::Success::FAILURE;
+    }
+    this->log_WARNING_LO_TcaUnhealthy_ThrottleClear();
+
+    if (!device_is_ready(this->m_mux)) {
+        this->log_WARNING_LO_MuxUnhealthy();
+        return Fw::Success::FAILURE;
+    }
+    this->log_WARNING_LO_MuxUnhealthy_ThrottleClear();
+
+    if (!this->loadSwitchReady()) {
+        this->log_WARNING_LO_LoadSwitchNotReady();
+        return Fw::Success::FAILURE;
+    }
+    this->log_WARNING_LO_LoadSwitchNotReady_ThrottleClear();
+
+    int rc = device_init(this->m_dev);
+    if (rc < 0) {
+        this->log_WARNING_LO_DeviceInitFailed(rc);
+        return Fw::Success::FAILURE;
+    }
+    this->log_WARNING_LO_DeviceInitFailed_ThrottleClear();
+
+    return Fw::Success::SUCCESS;
+}
+
+Fw::Success Tmp112Manager ::deinitializeDevice() {
+    if (!this->m_dev) {
+        this->log_WARNING_LO_DeviceNil();
+        return Fw::Success::FAILURE;
+    }
+    this->log_WARNING_LO_DeviceNil_ThrottleClear();
+
+    if (!this->m_dev->state) {
+        this->log_WARNING_LO_DeviceStateNil();
+        return Fw::Success::FAILURE;
+    }
+    this->log_WARNING_LO_DeviceStateNil_ThrottleClear();
+
+    this->m_dev->state->initialized = false;
+    return Fw::Success::SUCCESS;
+}
+
+bool Tmp112Manager ::loadSwitchReady() {
+    return (this->m_load_switch_state == Fw::On::ON && this->getTime() >= this->m_load_switch_on_timeout) ||
+           !this->m_load_switch_check;
+}
+
+}  // namespace Drv
---
+++ FprimeZephyrReference/Components/ThermalManager/ThermalManager.cpp
@@ -0,0 +1,38 @@
+// ======================================================================
+// \title  ThermalManager.cpp
+// \brief  cpp file for ThermalManager component implementation class
+// ======================================================================
+
+#include "FprimeZephyrReference/Components/ThermalManager/ThermalManager.hpp"
+
+#include <Fw/Types/Assert.hpp>
+
+namespace Components {
+
+// ----------------------------------------------------------------------
+// Component construction and destruction
+// ----------------------------------------------------------------------
+
+ThermalManager::ThermalManager(const char* const compName) : ThermalManagerComponentBase(compName) {}
+
+ThermalManager::~ThermalManager() {}
+
+// ----------------------------------------------------------------------
+// Handler implementations for typed input ports
+// ----------------------------------------------------------------------
+
+void ThermalManager::run_handler(FwIndexType portNum, U32 context) {
+    Fw::Success condition;
+
+    // Face temp sensors
+    for (FwIndexType i = 0; i < this->getNum_faceTempGet_OutputPorts(); i++) {
+        this->faceTempGet_out(i, condition);
+    }
+
+    // Battery cell temp sensors
+    for (FwIndexType i = 0; i < this->getNum_battCellTempGet_OutputPorts(); i++) {
+        this->battCellTempGet_out(i, condition);
+    }
+}
+
+}  // namespace Components
