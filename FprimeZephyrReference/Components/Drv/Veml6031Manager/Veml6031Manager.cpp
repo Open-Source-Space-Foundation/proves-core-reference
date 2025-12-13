e379d667f14b6bdd0cdc9ade36dd4bcc36a85eac
M Starch <LeStarch@googlemail.com>
2025-12-13 11:40:09 -0600

    Bump to fprime v4.1.1 (#204)
    * Bump to fprime v4.1.1
    
    * Remove load-switch not-ready

--- FprimeZephyrReference/Components/Drv/Veml6031Manager/Veml6031Manager.cpp
+++ FprimeZephyrReference/Components/Drv/Veml6031Manager/Veml6031Manager.cpp
@@ -140,10 +140,8 @@ Fw::Success Veml6031Manager ::initializeDevice() {
     this->log_WARNING_LO_MuxUnhealthy_ThrottleClear();
 
     if (!this->loadSwitchReady()) {
-        this->log_WARNING_LO_LoadSwitchNotReady();
         return Fw::Success::FAILURE;
     }
-    this->log_WARNING_LO_LoadSwitchNotReady_ThrottleClear();
 
     int rc = device_init(this->m_dev);
     if (rc < 0) {
