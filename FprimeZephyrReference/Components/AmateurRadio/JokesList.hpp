fe350ab88b4ddb23c597da70d22f70301532c6de
ineskhou <ineskhouider03@gmail.com>
2026-01-03 18:06:39 -0800

    shorten joke list

--- FprimeZephyrReference/Components/AmateurRadio/JokesList.hpp
+++ FprimeZephyrReference/Components/AmateurRadio/JokesList.hpp
@@ -7,22 +7,22 @@ namespace Components {
 
 static const char* const JOKES[] = {
     "I was once a Bronco, now I go Bonko",
-    "CATCH!!!",
-    "Don't look up...",
     "Wait... it's flat?",
-    "Hello down there!",
     "Mission Accomplished!! In a George W. kinda way...",
-    "(www.plushie link.com)",
-    "Bye Orbiter!",
+    "https://github.com/Open-Source-Space-Foundation/proves-core-reference",
     "I'm deorbiting to your location imminently",
     "My father was Mr. Cubesat, please call me Cubesatson",
-    "Hello, I am stuck inside this Cubesat",
+    "Help! I am stuck inside this Cubesat",
     "I can see my house from here",
     "Beep... Beep... Beep................ Boop?",
     "DOWN WITH THE GROUND STATION OPERATORS!!",
     "Made in China, assembled at Radio Shack",
-    "The Aurora Borealis? At this time of year? At this time of day? In this part of the orbit? Localized entirely around this satellite?"
-};
+    "The Aurora Borealis? At this time of year? At this time of day? In this part of the orbit? Localized entirely "
+    "around this satellite?",
+    "You lost the game.",
+    "I am not saying FSK modulation is the best way to send jokes, but at least it is never monotone!",
+    "Finally escaped the LA Traffic!",
+    "Hubble Deorbit Maneuver Activated!"};
 
 static const FwSizeType NUM_JOKES = 16;
 
