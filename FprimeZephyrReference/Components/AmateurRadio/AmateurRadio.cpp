bb27f827a15b24fde8cb07054dadb6d9eff9d5e5
Sam Yu <yudataguy@gmail.com>
2026-01-07 04:46:52 +0900

    Refactor jokes to use one event per joke to reduce stack usage

--- FprimeZephyrReference/Components/AmateurRadio/AmateurRadio.cpp
+++ FprimeZephyrReference/Components/AmateurRadio/AmateurRadio.cpp
@@ -6,32 +6,44 @@
 
 #include "FprimeZephyrReference/Components/AmateurRadio/AmateurRadio.hpp"
 
-#include <string>
-
-#include "FprimeZephyrReference/Components/AmateurRadio/JokesList.hpp"
 #include <zephyr/random/random.h>
+
 namespace Components {
 
 // ----------------------------------------------------------------------
 // Component construction and destruction
 // ----------------------------------------------------------------------
 
-AmateurRadio ::AmateurRadio(const char* const compName) : AmateurRadioComponentBase(compName), m_count_names(0) {}
+AmateurRadio::AmateurRadio(const char* const compName) : AmateurRadioComponentBase(compName), m_count_names(0) {}
 
-AmateurRadio ::~AmateurRadio() {}
+AmateurRadio::~AmateurRadio() {}
 
 // ----------------------------------------------------------------------
 // Handler implementations for commands
 // ----------------------------------------------------------------------
 
-void AmateurRadio ::TELL_JOKE_cmdHandler(FwOpcodeType opCode, U32 cmdSeq) {
-    // Get a random joke
-    const FwSizeType jokeIndex = sys_rand32_get() % NUM_JOKES;
-    const char* joke = JOKES[jokeIndex];
+void AmateurRadio::TELL_JOKE_cmdHandler(FwOpcodeType opCode, U32 cmdSeq) {
+    const U32 jokeIndex = sys_rand32_get() % NUM_JOKES;
 
-    // Log the joke as an event
-    Fw::LogStringArg jokeArg(joke);
-    this->log_ACTIVITY_HI_JokeTold(jokeArg);
+    switch (jokeIndex) {
+        case 0: this->log_ACTIVITY_HI_Joke0(); break;
+        case 1: this->log_ACTIVITY_HI_Joke1(); break;
+        case 2: this->log_ACTIVITY_HI_Joke2(); break;
+        case 3: this->log_ACTIVITY_HI_Joke3(); break;
+        case 4: this->log_ACTIVITY_HI_Joke4(); break;
+        case 5: this->log_ACTIVITY_HI_Joke5(); break;
+        case 6: this->log_ACTIVITY_HI_Joke6(); break;
+        case 7: this->log_ACTIVITY_HI_Joke7(); break;
+        case 8: this->log_ACTIVITY_HI_Joke8(); break;
+        case 9: this->log_ACTIVITY_HI_Joke9(); break;
+        case 10: this->log_ACTIVITY_HI_Joke10(); break;
+        case 11: this->log_ACTIVITY_HI_Joke11(); break;
+        case 12: this->log_ACTIVITY_HI_Joke12(); break;
+        case 13: this->log_ACTIVITY_HI_Joke13(); break;
+        case 14: this->log_ACTIVITY_HI_Joke14(); break;
+        case 15: this->log_ACTIVITY_HI_Joke15(); break;
+        default: this->log_ACTIVITY_HI_Joke0(); break;
+    }
 
     this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
 }
--- FprimeZephyrReference/Components/AmateurRadio/AmateurRadio.fpp
+++ FprimeZephyrReference/Components/AmateurRadio/AmateurRadio.fpp
@@ -9,10 +9,23 @@ module Components {
         @ Command to tell a random satellite-themed joke
         sync command TELL_JOKE()
 
-        @ Event with the joke text
-        event JokeTold(joke: string size 140) \
-            severity activity high id 1 \
-            format "{}"
+        @ Joke events - one per joke to avoid runtime string allocation
+        event Joke0() severity activity high format "I was once a Bronco, now I go Bonko"
+        event Joke1() severity activity high format "Wait... it's flat?"
+        event Joke2() severity activity high format "Mission Accomplished!! In a George W. kinda way..."
+        event Joke3() severity activity high format "https://github.com/Open-Source-Space-Foundation/proves-core-reference"
+        event Joke4() severity activity high format "I'm deorbiting to your location imminently"
+        event Joke5() severity activity high format "My father was Mr. Cubesat, please call me Cubesatson"
+        event Joke6() severity activity high format "Help! I am stuck inside this Cubesat"
+        event Joke7() severity activity high format "I can see my house from here"
+        event Joke8() severity activity high format "Beep... Beep... Beep................ Boop?"
+        event Joke9() severity activity high format "DOWN WITH THE GROUND STATION OPERATORS!!"
+        event Joke10() severity activity high format "Made in China, assembled at Radio Shack"
+        event Joke11() severity activity high format "The Aurora Borealis? At this time of year? At this time of day? In this part of the orbit? Localized entirely around this satellite?"
+        event Joke12() severity activity high format "You lost the game."
+        event Joke13() severity activity high format "I am not saying FSK modulation is the best way to send jokes, but at least it is never monotone!"
+        event Joke14() severity activity high format "Finally escaped the LA Traffic!"
+        event Joke15() severity activity high format "Hubble Deorbit Maneuver Activated!"
 
         @ Amount of tags this component has goten
         telemetry count_names: U32
