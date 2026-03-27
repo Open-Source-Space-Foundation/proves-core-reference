81271b209517f0904f0571946f69807fd87bd99f
James Cowley <wuz7@txstate.edu>
2026-03-27 18:46:15 -0500

    pull out threshold values

--- PROVESFlightControllerReference/Components/ThermalManager/ThermalManager.cpp
+++ PROVESFlightControllerReference/Components/ThermalManager/ThermalManager.cpp
@@ -25,19 +25,22 @@ void ThermalManager::run_handler(FwIndexType portNum, U32 context) {
     Fw::Success condition;
     Fw::ParamValid param_valid;
 
+    const F64 FACE_TEMP_LOWER_THRESHOLD = this->paramGet_FACE_TEMP_LOWER_THRESHOLD(param_valid);
+    const F64 FACE_TEMP_UPPER_THRESHOLD = this->paramGet_FACE_TEMP_UPPER_THRESHOLD(param_valid);
+    const F64 BATT_CELL_TEMP_LOWER_THRESHOLD = this->paramGet_BATT_CELL_TEMP_LOWER_THRESHOLD(param_valid);
+    const F64 BATT_CELL_TEMP_UPPER_THRESHOLD = this->paramGet_BATT_CELL_TEMP_UPPER_THRESHOLD(param_valid);
+
     // Face temp sensors
     for (FwIndexType i = 0; i < this->getNum_faceTempGet_OutputPorts(); i++) {
         F64 temperature = this->faceTempGet_out(i, condition);
-        if (temperature < this->paramGet_FACE_TEMP_LOWER_THRESHOLD(param_valid)) {
+        if (temperature < FACE_TEMP_LOWER_THRESHOLD) {
             this->log_WARNING_LO_FaceTemperatureBelowThreshold(i, temperature);
-        } else if (temperature >
-                   this->paramGet_FACE_TEMP_LOWER_THRESHOLD(param_valid) + ThermalManager::DEBOUNCE_ERROR) {
+        } else if (temperature > FACE_TEMP_LOWER_THRESHOLD + ThermalManager::DEBOUNCE_ERROR) {
             this->log_WARNING_LO_FaceTemperatureBelowThreshold_ThrottleClear();
         }
-        if (temperature > this->paramGet_FACE_TEMP_UPPER_THRESHOLD(param_valid)) {
+        if (temperature > FACE_TEMP_UPPER_THRESHOLD) {
             this->log_WARNING_LO_FaceTemperatureAboveThreshold(i, temperature);
-        } else if (temperature <
-                   this->paramGet_FACE_TEMP_UPPER_THRESHOLD(param_valid) - ThermalManager::DEBOUNCE_ERROR) {
+        } else if (temperature < FACE_TEMP_UPPER_THRESHOLD - ThermalManager::DEBOUNCE_ERROR) {
             this->log_WARNING_LO_FaceTemperatureAboveThreshold_ThrottleClear();
         }
     }
@@ -45,16 +48,14 @@ void ThermalManager::run_handler(FwIndexType portNum, U32 context) {
     // Battery cell temp sensors
     for (FwIndexType i = 0; i < this->getNum_battCellTempGet_OutputPorts(); i++) {
         F64 temperature = this->battCellTempGet_out(i, condition);
-        if (temperature < this->paramGet_BATT_CELL_TEMP_LOWER_THRESHOLD(param_valid)) {
+        if (temperature < BATT_CELL_TEMP_LOWER_THRESHOLD) {
             this->log_WARNING_LO_BatteryCellTemperatureBelowThreshold(i, temperature);
-        } else if (temperature >
-                   this->paramGet_BATT_CELL_TEMP_LOWER_THRESHOLD(param_valid) + ThermalManager::DEBOUNCE_ERROR) {
+        } else if (temperature > BATT_CELL_TEMP_LOWER_THRESHOLD + ThermalManager::DEBOUNCE_ERROR) {
             this->log_WARNING_LO_BatteryCellTemperatureBelowThreshold_ThrottleClear();
         }
-        if (temperature > this->paramGet_BATT_CELL_TEMP_UPPER_THRESHOLD(param_valid)) {
+        if (temperature > BATT_CELL_TEMP_UPPER_THRESHOLD) {
             this->log_WARNING_LO_BatteryCellTemperatureAboveThreshold(i, temperature);
-        } else if (temperature <
-                   this->paramGet_BATT_CELL_TEMP_UPPER_THRESHOLD(param_valid) - ThermalManager::DEBOUNCE_ERROR) {
+        } else if (temperature < BATT_CELL_TEMP_UPPER_THRESHOLD - ThermalManager::DEBOUNCE_ERROR) {
             this->log_WARNING_LO_BatteryCellTemperatureAboveThreshold_ThrottleClear();
         }
     }
