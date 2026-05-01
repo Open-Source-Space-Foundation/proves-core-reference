26d58a724f46c451fb5394cbb11bdaa88711896a
Nate Gay <email@nategay.me>
2026-04-30 23:29:42 -0500

    Add comments

--- PROVESFlightControllerReference/Components/PacketProcessor/Validator.hpp
+++ PROVESFlightControllerReference/Components/PacketProcessor/Validator.hpp
@@ -16,10 +16,10 @@ namespace PacketValidator {
 //! Status of validation attempt
 //! Must match the ValidatorStatus enum in the .fpp file
 enum class Status {
-    Valid,
-    Bypass,
-    SpiInvalid,
-    SequenceNumberOutOfWindow,
+    Valid,                      //!< The packet is valid and passes all checks
+    Bypass,                     //!< The packet is allowed to bypass authentication based on its OpCode
+    SpiInvalid,                 //!< The packet SPI field is invalid
+    SequenceNumberOutOfWindow,  //!< The packet sequence number is outside the acceptable window
 };
 
 }  // namespace PacketValidator
