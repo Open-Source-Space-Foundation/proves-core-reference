dc987080973a524a7102f2d183a5e7d45e09aad9
Collin <collincook5804@gmail.com>
2026-07-25 22:05:03 -0500

    Don't set proc time if dev not ready

--- PROVESFlightControllerReference/Components/Drv/RtcManager/RtcManager.cpp
+++ PROVESFlightControllerReference/Components/Drv/RtcManager/RtcManager.cpp
@@ -59,7 +59,7 @@ void RtcManager ::timeGetPort_handler(FwIndexType portNum, Fw::Time& time) {
     // Check device readiness
     if (!device_is_ready(this->m_dev)) {
         this->log_CONSOLE_RtcNotReady();
-        this->m_ProcTimeSet = true;  // proc time flag
+        // Don't set the proc time flag if device is not ready
 
         // Use uptime as fallback
         time.set(TimeBase::TB_PROC_TIME, 0, seconds_since_boot, useconds_since_boot);
