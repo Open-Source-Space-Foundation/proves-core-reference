320727bc413a1e866f88dec4c5117b1ba1e0c598
Collin <collincook5804@gmail.com>
2026-07-31 19:05:55 -0500

    testing

--- PROVESFlightControllerReference/test/int/rtc_test.py
+++ PROVESFlightControllerReference/test/int/rtc_test.py
@@ -443,16 +443,15 @@ def test_11_proc_toggle(fprime_test_api: IntegrationTestAPI, start_gds):
     """Test for events emitted by proc time toggle"""
 
     # Test that we can set timebase to proc time
-    # fprime_test_api.send_command(f"{rtcManager}.SET_TIMEBASE", [1])
-    # # Assert that we receive a TimeBase event within 10 seconds
-    # fprime_test_api.assert_event(f"{rtcManager}.TimeBase", start="NOW", timeout=10)
+    fprime_test_api.send_command(f"{rtcManager}.TIMEBASE_PRM_SET", ["TB_PROC_TIME"])
+    # Assert that we receive a TimeBase event within 10 seconds
+    fprime_test_api.assert_event(
+        f"{rtcManager}.TimeBaseSwitch", start="NOW", timeout=10
+    )
 
-    # # Test that we can set timebase to RTC time
-    # fprime_test_api.send_command(f"{rtcManager}.SET_TIMEBASE", [3])
-    # # Assert that we receive a TimeBase event within 10 seconds
-    # fprime_test_api.assert_event(f"{rtcManager}.TimeBase", start="NOW", timeout=10)
-
-    # # Test that invalid times are filtered out
-    # fprime_test_api.send_command(f"{rtcManager}.SET_TIMEBASE", [0])
-    # # Assert that we receive a TimeBaseInvalid event within 10 seconds
-    # fprime_test_api.assert_event(f"{rtcManager}.InvalidTimeBase", start="NOW", timeout=10)
+    # Test that we can set timebase to RTC time
+    fprime_test_api.send_command(f"{rtcManager}.TIMEBASE_PRM_SET", ["TB_SC_TIME"])
+    # Assert that we receive a TimeBase event within 10 seconds
+    fprime_test_api.assert_event(
+        f"{rtcManager}.TimeBaseSwitch", start="NOW", timeout=10
+    )
